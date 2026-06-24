74bd1e68e984acd885f5adfdb8906067f2935c50
Austin "Paco" Rainwater <hey@paco.wtf>
2026-06-24 16:02:53 -0400

    Try a small change

--- terraform/lab/test_user.tf
+++ terraform/lab/test_user.tf
@@ -1,6 +1,6 @@
 # Test user -- will be deleted
 resource "proxmox_virtual_environment_user" "test" {
-  comment = "Test user managed by terraform -- do not use"
+  comment = "Test user managed by the Terraform -- do not use"
   user_id = "fakeuser@pve"
   enabled = false
 }
