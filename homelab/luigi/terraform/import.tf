67661c6b604b8cd309ad91f9b40890c5283bbff0
Austin "Paco" Rainwater <hey@paco.wtf>
2026-06-25 15:31:49 -0400

    Correct image id

--- homelab/luigi/terraform/import.tf
+++ homelab/luigi/terraform/import.tf
@@ -1,4 +1,4 @@
 import {
   to = proxmox_download_file.alpine_template
-  id = "local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz"
+  id = "rainbow_road/local:vztmpl/alpine-3.22-default_20250617_amd64.tar.xz"
 }
